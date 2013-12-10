abd5c01811c2b7c72d3fee970b44e528ce759fe6
Arthur Kay <arthurakay@gmail.com>
2013-12-10 11:00:41 -0600

    A bit of cleanup before moving forward.

--- devtools-page.js
+++ devtools-page.js
@@ -3,53 +3,53 @@
 /**
  * Sencha Panel
  */
-chrome.devtools.panels.create("Sencha", "resources/images/panel_icon.png", "inspector.html", function() {});
+chrome.devtools.panels.create("Sencha", "resources/images/panel_icon.png", "workspace/index.html", function() {});
 
 /**
  * Elements Side Panel
  */
-var elementsPanel = chrome.devtools.panels.elements;
-
-elementsPanel.createSidebarPane("Sencha Component", function(sidebar) {
-    function onSelectionChanged() {
-        sidebar.setExpression("(" + pageDetectSenchaComponent.toString() + ")()");
-    }
-    onSelectionChanged();
-
-    // selection listener
-    elementsPanel.onSelectionChanged.addListener(onSelectionChanged);
-});
-
-function pageDetectSenchaComponent() {
-    var cmp, data, cmpRef, xtype, 
-        selectedEl = $0,
-        ref = '';
-    
-    if (window.Ext) {
-        cmp = Ext.getCmp(selectedEl.id);
-    
-        if (cmp) {
-            data = {__proto__: null};
-            
-            // class name
-            if (Ext.getClassName) {
-                ref = Ext.getClassName(cmp);
-            }
-
-            // xtype
-            xtype = cmp.xtype||(cmp.getXType ? cmp.getXType() : '');
-            if (xtype) {
-                ref += ' ('+ xtype +')';
-            }
-
-            if (!ref) {
-                ref = '#' + cmp.id;
-            }
-
-            data[ref] = cmp;
-        }
-    
-    }
-
-    return data;
-}
+//var elementsPanel = chrome.devtools.panels.elements;
+//
+//elementsPanel.createSidebarPane("Sencha Component", function(sidebar) {
+//    function onSelectionChanged() {
+//        sidebar.setExpression("(" + pageDetectSenchaComponent.toString() + ")()");
+//    }
+//    onSelectionChanged();
+//
+//    // selection listener
+//    elementsPanel.onSelectionChanged.addListener(onSelectionChanged);
+//});
+//
+//function pageDetectSenchaComponent() {
+//    var cmp, data, cmpRef, xtype,
+//        selectedEl = $0,
+//        ref = '';
+//
+//    if (window.Ext) {
+//        cmp = Ext.getCmp(selectedEl.id);
+//
+//        if (cmp) {
+//            data = {__proto__: null};
+//
+//            // class name
+//            if (Ext.getClassName) {
+//                ref = Ext.getClassName(cmp);
+//            }
+//
+//            // xtype
+//            xtype = cmp.xtype||(cmp.getXType ? cmp.getXType() : '');
+//            if (xtype) {
+//                ref += ' ('+ xtype +')';
+//            }
+//
+//            if (!ref) {
+//                ref = '#' + cmp.id;
+//            }
+//
+//            data[ref] = cmp;
+//        }
+//
+//    }
+//
+//    return data;
+//}
--- inspector.js
+++
@@ -1,8 +0,0 @@
-/**
- * Page Load
- */
-window.addEventListener('DOMContentLoaded', function() {
-    AppInspector.InspectedWindow.checkAPI(function() {
-        new AppInspector.Viewport();
-    });
-});
