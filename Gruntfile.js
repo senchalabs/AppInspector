b9f6a0235c14757c2054137ae1b9a1bd0b2e19fe
Stefan Stölzle <stefan@stoelzle.me>
2014-01-21 19:20:08 +1100

    better output for jshint task
    reporter jshint-stylish added

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,8 @@ module.exports = function(grunt) {
         },
         jshint: {
             options: {
-                jshintrc: '.jshintrc'
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
             },
             all: [
                 'Gruntfile.js',
