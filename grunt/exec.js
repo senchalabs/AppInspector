7c20cdaa6db810163ac57ba9b01e3cd8cd43fd4c
Stefan Stölzle <stefan@stoelzle.me>
2014-02-25 14:04:36 +1100

    exec task test
    added a test to the exec task (grunt/exec.js) to check if the app/AppInspector/.sencha/ folder exists that is needed for the build

--- grunt/exec.js
+++ grunt/exec.js
@@ -1,6 +1,36 @@
-module.exports = {
-    production: {
-        cwd: '<%= yeoman.app %>/AppInspector/',
-        command: 'sencha -q app build -e production'
-    }
+var fs = require('fs'),
+    chalk = require('chalk'),
+    senchadir = './app/AppInspector/.sencha/';
+
+module.exports = function(grunt) {
+    // look up if AppInspector was opened once in Sencha Architect
+    // if so »./app/AppInspector/.sencha/« will exist containing build property files
+    fs.exists(senchadir, function(exists) {
+        var sa = {
+                name: chalk.green('Sencha Architect'),
+                path: chalk.yellow('./path/to/project/app/AppInspector/AppInspector.xds')
+            };
+
+        // if not, fail task
+        if (!exists) {
+            //
+            grunt.fail.fatal([
+                chalk.white(chalk.bold.blue(senchadir) + ' folder not found!'),
+                '',
+                '    To fix this, please open the project in ' + sa.name + ' first and save once.',
+                '    You can find the ' + sa.name + ' project under ' + sa.path
+            ].join('\n'));
+        }
+    });
+    // else return task config
+    return {
+        testing: {
+            cwd: '<%= yeoman.app %>/AppInspector/',
+            command: 'sencha -q app build -e testing'
+        },
+        production: {
+            cwd: '<%= yeoman.app %>/AppInspector/',
+            command: 'sencha -q app build -e production'
+        }
+    };
 };
