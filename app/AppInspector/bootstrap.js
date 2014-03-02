f3b838e21bbfbc0fc9007182eed36095d6b4b697
Stefan Stölzle <stefan@stoelzle.me>
2014-03-02 17:31:38 +1100

    added indicator on disabled devtools when no Sencha framework found

--- app/AppInspector/bootstrap.js
+++ app/AppInspector/bootstrap.js
@@ -461,7 +461,6 @@ Ext.ClassManager.addNameAlternateMappings({
   ],
   "Ext.grid.RowEditor": [],
   "Ext.grid.RowEditorButtons": [],
-  "Ext.grid.Scroller": [],
   "Ext.grid.View": [],
   "Ext.grid.ViewDropZone": [],
   "Ext.grid.column.Action": [
@@ -1377,7 +1376,6 @@ Ext.ClassManager.addNameAliasMappings({
   "Ext.grid.RowEditorButtons": [
     "widget.roweditorbuttons"
   ],
-  "Ext.grid.Scroller": [],
   "Ext.grid.View": [
     "widget.gridview"
   ],
