34cd97aa83c58b81607c89b301aa40435ed8d8a4
Arthur Kay <arthurakay@gmail.com>
2014-03-25 10:45:15 -0500

    Fixing issue #42 - component highlighting

--- app/AppInspector/app/util/InspectedWindow.js
+++ app/AppInspector/app/util/InspectedWindow.js
@@ -2,39 +2,59 @@
  *
  */
 Ext.define('AI.util.InspectedWindow', {
-    singleton: true,
+    singleton : true,
 
-    requires: [
+    requires : [
         'AI.util.Error'
     ],
 
     /**
      * @param {String} id
      */
-    highlight: function(id) {
+    highlight : function (id) {
         var cmp = Ext.getCmp(id),
             el = document.getElementById('_AppInspector'),
-            box;
+            box, cmpDom;
 
         if (cmp && cmp.rendered) {
             //Ext JS
             if (cmp.el) {
                 box = cmp.el.getBox();
+                cmpDom = cmp.el.dom;
             }
 
             //Touch
             else if (cmp.element) {
                 box = cmp.element.getBox();
+                cmpDom = cmp.element.dom;
             }
 
-            el.style.height = box.height + 'px';
-            el.style.width = box.width + 'px';
-            el.style.top = box.top + 'px';
-            el.style.left = box.left + 'px';
-            el.style.visibility = '';
-            el.style.zIndex = 99999;
+            Ext.apply(el.style, {
+                height : box.height + 'px',
+                width  : box.width + 'px',
 
-            window.setTimeout(function() {
+                visibility : '',
+                zIndex     : 99999
+            });
+
+            var getPosition = function (element) {
+                var xPosition = 0;
+                var yPosition = 0;
+
+                while(element) {
+                    xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+                    yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+                    element = element.offsetParent;
+                }
+                return {
+                    left : xPosition + 'px',
+                    top  : yPosition + 'px'
+                };
+            };
+
+            Ext.apply(el.style, getPosition(cmpDom));
+
+            window.setTimeout(function () {
                 var el = document.getElementById('_AppInspector');
 
                 el.style.visibility = 'hidden';
@@ -51,11 +71,11 @@ Ext.define('AI.util.InspectedWindow', {
      *
      * - isException {Boolean}: whether-or-not the closure function encountered an exception
      */
-    eval: function(closure, argString, callback) {
+    eval : function (closure, argString, callback) {
         var callbackFn = callback,
             args = '';
 
-        var encodeArg = function(arg) {
+        var encodeArg = function (arg) {
             switch (typeof arg) {
                 case 'string':
                     return '"' + arg + '"';
@@ -73,7 +93,7 @@ Ext.define('AI.util.InspectedWindow', {
             args = encodeArg(argString);
         }
         else if (argString) {
-            Ext.Array.each(argString, function(x) {
+            Ext.Array.each(argString, function (x) {
                 if (args !== '') {
                     args += ', ';
                 }
@@ -84,7 +104,7 @@ Ext.define('AI.util.InspectedWindow', {
 
         chrome.devtools.inspectedWindow.eval(
             '(' + closure + ')(' + args + ')',
-            function(result, isException) {
+            function (result, isException) {
                 if (isException) {
                     AI.util.Error.parseException(isException);
                     return;
@@ -98,22 +118,22 @@ Ext.define('AI.util.InspectedWindow', {
     /**
      * Function to get the framework and version of the inspected app.
      */
-    getAppVersion: function() {
+    getAppVersion : function () {
         if (!window.Ext) {
             return false;
         }
 
         //helper class
         Ext.define('Ext.ux.AppInspector', {
-            singleton: true,
+            singleton      : true,
 
             //for the Event Monitor
-            eventCache: null,
-            eventCaptureFn: null,
+            eventCache     : null,
+            eventCaptureFn : null,
 
             //for the right-click menu
-            contextRef: null,
-            contextFn: function(evt, target, eOpts) {
+            contextRef     : null,
+            contextFn      : function (evt, target, eOpts) {
                 var cmp = Ext.getCmp(target.id);
 
                 if (cmp) {
